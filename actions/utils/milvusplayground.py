526cea2c3601b1a5f15713a11d2ec58c1e7bd0c8
Fabian <fabianfey@web.de>
2023-07-27 18:20:55 +0200

    Added sentence embeddings to the mix :D

--- actions/utils/milvusplayground.py
+++ actions/utils/milvusplayground.py
@@ -1,3 +1,6 @@
+import logging as logger
+logger.basicConfig(level=logger.INFO)
+
 from pymilvus import (
     connections,
     utility,
@@ -7,43 +10,70 @@ from pymilvus import (
     Collection,
 )
 
-connections.connect("default", host="milvus-standalone", port="19530")
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+import numpy as np
 
-fields = [
-    FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=False),
-    FieldSchema(name="random", dtype=DataType.DOUBLE),
-    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=8)
-]
-schema = CollectionSchema(fields, "hello_milvus is the simplest demo to introduce the APIs")
-hello_milvus = Collection("hello_milvus", schema)
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
 
-import random
-entities = [
-    [i for i in range(3000)],  # field pk
-    [float(random.randrange(-20, -10)) for _ in range(3000)],  # field random
-    [[random.random() for _ in range(8)] for _ in range(3000)],  # field embeddings
-]
-insert_result = hello_milvus.insert(entities)
-hello_milvus.flush()  
+english_sentences = ["dog", "Puppies are nice.", "I enjoy taking long walks along the beach with my dog."]
+italian_sentences = ["cane", "I cuccioli sono carini.", "Mi piace fare lunghe passeggiate lungo la spiaggia con il mio cane."]
+japanese_sentences = ["犬", "子犬はいいです", "私は犬と一緒にビーチを散歩するのが好きです"]
 
-index = {
-    "index_type": "IVF_FLAT",
-    "metric_type": "L2",
-    "params": {"nlist": 128},
-}
-hello_milvus.create_index("embeddings", index)
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
 
-hello_milvus.load()
-vectors_to_search = entities[-1][-2:]
-search_params = {
-    "metric_type": "L2",
-    "params": {"nprobe": 10},
-}
-result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, output_fields=["random"])
-print(result)
+# Compute embeddings.
+en_result = embed(english_sentences)
+print(en_result)
+it_result = embed(italian_sentences)
+ja_result = embed(japanese_sentences)
 
-result = hello_milvus.query(expr="random > -14", output_fields=["random", "embeddings"])
-print(result)
+# Compute similarity matrix. Higher score indicates greater similarity.
+similarity_matrix_it = np.inner(en_result, it_result)
+similarity_matrix_ja = np.inner(en_result, ja_result)
 
-result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, expr="random > -12", output_fields=["random"])
-print(result)
+print(similarity_matrix_it)
+print(similarity_matrix_ja)
+
+
+# connections.connect("default", host="milvus-standalone", port="19530")
+
+# fields = [
+#     FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=False),
+#     FieldSchema(name="random", dtype=DataType.DOUBLE),
+#     FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=8)
+# ]
+# schema = CollectionSchema(fields, "hello_milvus is the simplest demo to introduce the APIs")
+# hello_milvus = Collection("hello_milvus", schema)
+
+# import random
+# entities = [
+#     [i for i in range(3000)],  # field pk
+#     [float(random.randrange(-20, -10)) for _ in range(3000)],  # field random
+#     [[random.random() for _ in range(8)] for _ in range(3000)],  # field embeddings
+# ]
+# insert_result = hello_milvus.insert(entities)
+# hello_milvus.flush()  
+
+# index = {
+#     "index_type": "IVF_FLAT",
+#     "metric_type": "L2",
+#     "params": {"nlist": 128},
+# }
+# hello_milvus.create_index("embeddings", index)
+
+# hello_milvus.load()
+# vectors_to_search = entities[-1][-2:]
+# search_params = {
+#     "metric_type": "L2",
+#     "params": {"nprobe": 10},
+# }
+# result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, output_fields=["random"])
+# print(result)
+
+# result = hello_milvus.query(expr="random > -14", output_fields=["random", "embeddings"])
+# print(result)
+
+# result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, expr="random > -12", output_fields=["random"])
+# print(result)
